77ca0173bf97c5a9d20a1d1628bffe0f88b8aae3
kyunghoRyu <kyungho.ryu95@gmail.com>
2020-10-23 15:57:40 +0900

    2020/10/23

---
+++ assets/js/plugins/markerwidget.js
@@ -0,0 +1,13 @@
+   (function(d, h, m){
+        var js, fjs = d.getElementsByTagName(h)[0];
+        if (d.getElementById(m)){return;}
+        js = d.createElement(h); js.id = m;
+        js.onload = function(){
+            window.makerWidgetComInit({
+            position: "left",          
+            widget: "jb9wwhnnn9jajivg-jn5j85rqnvatchgt-pe7uz6m7vvmi2up8"                
+        })};
+        js.src = "https://makerwidget.com/js/embed.js";
+        fjs.parentNode.insertBefore(js, fjs)
+    }(document, "script", "dhm"));
+
